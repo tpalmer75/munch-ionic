0afd4ed29572cba693479812f56a0f5d22ce6630
lucid-taylor <taylorpalmer@Taylors-MacBook-Pro.local>
2015-07-01 12:58:29 -0600

    Added ngCordova
    Status Bar still doesn’t work

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var myApp = angular.module('starter', ['ionic', 'ionic-material'])
+var myApp = angular.module('starter', ['ionic', 'ionic-material', 'ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -12,22 +12,24 @@ var myApp = angular.module('starter', ['ionic', 'ionic-material'])
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
+
+    if (window.StatusBar) {
+      StatusBar.backgroundColorByHexString("#FF000");
     }
+
   });
 })
 
 // Trying to fix slow scrolling
-myApp.config(function($ionicConfigProvider) {
+//myApp.config(function($ionicConfigProvider) {
   // $ionicConfigProvider.scrolling.jsScrolling(false);
-});
+//});
 
 // Customize status bar
-//myApp.run(function($cordovaStatusbar) {
-  //$cordovaStatusbar.overlaysWebView(true)
-  //$cordovaStatusbar.styleHex('#679330');
-//});
+// .run(function($cordovaStatusbar) {
+//   $cordovaStatusbar.styleHex('#FF0000') //red
+// })
+
 
 // myApp.config(['$ionicConfigProvider', function($ionicConfigProvider) {
 
