5c790701dbfa99af059f135daa260c66b864f4c4
lucid-taylor <taylorpalmer@Taylors-Lucid-Macbook-Pro.local>
2015-10-01 13:02:25 -0600

    Tap to delete
    On the meal-detail.html template

--- www/js/app.js
+++ www/js/app.js
@@ -23,69 +23,135 @@ angular.module('munchApp', ['ionic', 'starter.controllers', 'starter.services'])
   });
 })
 
-.directive('openOptions', ['$ionicGesture', function($ionicGesture) {
+ .directive('openOptions', ['$ionicGesture', function($ionicGesture) {
   return {
     restrict: 'A',
     link: function (scope, element, attrs) {
       $ionicGesture.on('tap', function(e){
+        var ITEM_CONTENT_CLASS = 'item-content';
+  var ITEM_SLIDING_CLASS = 'item-sliding';
+  var ITEM_OPTIONS_CLASS = 'item-options';
 
-        // Grab the content
-        var content = element.parent()[0];
+        var content, buttons, offsetX, buttonsWidth;
 
-        // Grab the buttons and their width
-        var buttons = element.parent().parent()[0].querySelector('.item-options');
+        if (e.target.classList.contains(ITEM_CONTENT_CLASS)) {
+          content = e.target;
+        } else if (e.target.classList.contains(ITEM_CLASS)) {
+          content = e.target.querySelector('.' + ITEM_CONTENT_CLASS);
+        } else {
+          content = ionic.DomUtil.getParentWithClass(e.target, ITEM_CONTENT_CLASS);
+        }
 
-        if (!buttons) {
-          console.log('There are no option buttons');
+        // If we don't have a content area as one of our children (or ourselves), skip
+        if (!content) {
           return;
         }
-        var buttonsWidth = buttons.offsetWidth;
 
-        ionic.requestAnimationFrame(function() {
-          content.style[ionic.CSS.TRANSITION] = 'all ease-out .25s';
+        // Make sure we aren't animating as we slide
+        content.classList.remove(ITEM_SLIDING_CLASS);
+        console.log(content);
 
+        offsetX = parseFloat(content.style[ionic.CSS.TRANSFORM].replace('translate3d(', '').split(',')[0]) || 0;
 
-            buttons.classList.remove('invisible');
-            content.style[ionic.CSS.TRANSFORM] = 'translate3d(-' + buttonsWidth + 'px, 0, 0)';
-        });   
+        console.log("offset: " + offsetX);
 
+        // Grab the buttons
+        buttons = content.parentNode.querySelector('.' + ITEM_OPTIONS_CLASS);
+        if (!buttons) {
+          return;
+        }
+        
+        buttons.classList.remove('invisible');
+        buttonsWidth = buttons.offsetWidth;
+        
+        // El item está cerrado
+        if(offsetX === 0) {
+          content.style[ionic.CSS.TRANSFORM] = "translate3d(-" + buttonsWidth + "px, 0px, 0px)";
+        }
+        
+        // El item está cerrado
+        else {
+          content.style[ionic.CSS.TRANSFORM] = "translate3d(0px, 0px, 0px)";
+        }
+        
+        console.log("buttonsWidth: " + buttonsWidth);
+        
       }, element);
     }
   };
 }])
 
-.directive('closeOptions', ['$ionicGesture', function($ionicGesture) {
-  return {
-    restrict: 'A',
-    link: function (scope, element, attrs) {
-      $ionicGesture.on('tap', function(e){
 
-        // Grab the content
-        var content = element.parent()[0];
+// .directive('openOptions', ['$ionicGesture', function($ionicGesture) {
+//   return {
+//     restrict: 'A',
+//     link: function (scope, element, attrs) {
+//       $ionicGesture.on('tap', function(e){
 
-        // Grab the buttons and their width
-        var buttons = element.parent().parent()[0].querySelector('.item-options');
+//         // Grab the content
+//         var content = element.parent()[0];
 
-        if (!buttons) {
-          console.log('There are no option buttons');
-          return;
-        }
-        var buttonsWidth = buttons.offsetWidth;
+//         // Grab the buttons and their width
+//         var buttons = element.parent().parent()[0].querySelector('.item-options');
 
-        ionic.requestAnimationFrame(function() {
-          content.style[ionic.CSS.TRANSITION] = 'all ease-out .25s';
+//         var input = element.parent()[0].querySelector('input');
 
-            console.log('close');
-            content.style[ionic.CSS.TRANSFORM] = '';
-            setTimeout(function() {
-              buttons.classList.add('invisible');
-            }, 250);        
-        });   
+//         angular.element(input).attr('readonly', 'readonly');
 
-      }, element);
-    }
-  };
-}])
+
+//         //document.getElementById('foo').readOnly = true;
+
+//         if (!buttons) {
+//           console.log('There are no option buttons');
+//           return;
+//         }
+//         var buttonsWidth = buttons.offsetWidth;
+
+//         ionic.requestAnimationFrame(function() {
+//           //content.style[ionic.CSS.TRANSITION] = 'all ease-out .25s';
+
+
+//             buttons.classList.remove('invisible');
+//             content.style[ionic.CSS.TRANSFORM] = 'translate3d(-' + buttonsWidth + 'px, 0, 0)';
+//         });   
+
+//       }, element);
+//     }
+//   };
+// }])
+
+// .directive('closeOptions', ['$ionicGesture', function($ionicGesture) {
+//   return {
+//     restrict: 'A',
+//     link: function (scope, element, attrs) {
+//       $ionicGesture.on('swipe', function(e){
+
+//         // Grab the content
+//         var content = element.parent()[0];
+
+//         // Grab the buttons and their width
+//         var buttons = element.parent().parent()[0].querySelector('.item-options');
+
+//         if (!buttons) {
+//           console.log('There are no option buttons');
+//           return;
+//         }
+//         var buttonsWidth = buttons.offsetWidth;
+
+//         ionic.requestAnimationFrame(function() {
+//          //content.style[ionic.CSS.TRANSITION] = 'all ease-out .25s';
+
+//             console.log('close');
+//             content.style[ionic.CSS.TRANSFORM] = '';
+//             setTimeout(function() {
+//               buttons.classList.add('invisible');
+//             }, 250);        
+//         });   
+
+//       }, element);
+//     }
+//   };
+// }])
 
 .config(function($stateProvider, $urlRouterProvider) {
 
